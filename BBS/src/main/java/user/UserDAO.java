6890184d2ad5aa9e3ddfea3d162451b98b192f78
seowonme <86937748+seowonme@users.noreply.github.com>
2021-07-08 02:40:50 +0900

    회원가입 기능 구현하기
    데이터베이스 오류 잡기 확인.
    회원가입 관련 jsp 생성.

--- BBS/src/main/java/user/UserDAO.java
+++ BBS/src/main/java/user/UserDAO.java
@@ -5,6 +5,11 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Connection;
 
+//Data Access Object의 약자로 DataBase의 data에 접근을 위한 객체
+//DataBase에 접근을 하기위한 로직과 비즈니스 로직을 분리하기 위해서 사용
+//ConnectionPool : connection 객체를 미리 만들어 놓고 그것을 가져다 쓰는 것
+//다쓰고 반환해 놓는 것
+
 public class UserDAO {
 	
 	private Connection conn; //데이터베이스에 접근하게 해주는 객체
@@ -14,35 +19,49 @@ public class UserDAO {
 	//mysql에 접속을 하게 해주는 부분
 	public UserDAO() {
 		try {
-			String dbURL = "jdbc:mysql://localhost:3306/bbs?severTimezone=UTC;";
+			String dbURL =  "jdbc:mysql://localhost:3306/bbs?characterEncoding=UTF-8&serverTimezone=UTC";
 			String dbID = "study";
 			String dbPassword = "1234";
-			Class.forName("com.mysql.jdbc.cj.Driver");
-			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
-		}catch (Exception e){
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager.getConnection(dbURL,dbID,dbPassword);
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
 	public int login(String userID, String userPassword) {
-		String SQL = "SELECT userPassword FROM WHERE userID = ?";
+		String SQL = "SELECT userPassword FROM USER WHERE userID = ?";
 		try {
-			pstmt = conn.prepareStatement(SQL); 
+			pstmt = conn.prepareStatement(SQL);
 			pstmt.setString(1, userID);
 			rs = pstmt.executeQuery();
-			if(rs.next()) {
-				if(rs.getString(1).equals(userPassword)) { 
-					//결과로 나온userPassword를 받아서 접속을 시도한 userPassword와 동일하다면 1을 반환 한다.
+			if (rs.next()) {
+				if(rs.getString(1).equals(userPassword))
 					return 1; //로그인 성공
-				}else 
+				else
 					return 0; //비밀번호 불일치
 			}
 			return -1; //아이디가 없음
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -2; //데이터베이스 오류
+	}
+	
+	public int join(User user) {
+		String SQL = "INSERT INTO USER VALUES (?, ?, ?, ?, ?)";
+		try {
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, user.getUserID());
+			pstmt.setString(2, user.getUserPassword());
+			pstmt.setString(3, user.getUserName());
+			pstmt.setString(4, user.getUserGender());
+			pstmt.setString(5, user.getUserEmail());
+			return pstmt.executeUpdate(); //해당 statment 실행한 결과를 넣을 수 있도록
 		}catch (Exception e){
 			e.printStackTrace();
 		}
-		
-		return -2; //데이터베이스 오류를 의미
+		return -1; //데이터베이스 오류
 	}
 
 }
--- BBS/src/main/webapp/loginAction.jsp
+++ BBS/src/main/webapp/loginAction.jsp
@@ -14,31 +14,34 @@
 </head>
 <body>
      <% 
-        UserDAO userDAO = new UserDAO();
-        int result = userDAO.login(user.getUserID(), user.getUserPassword());
-        if(result == 1){
-        	PrintWriter script = response.getWriter();
-        	script.println("<script>");
-        	script.println("location.href = 'main.jsp'");
-        	script.println("</script>");
-        }else if(result == 0){
-        	PrintWriter script = response.getWriter();
-        	script.println("<script>");
-        	script.println("alert('비밀번호가 틀립니다.')");
-        	script.println("history.back()"); //이전 페이지로 사용자를 돌려보냄  
-        	script.println("</script>");
-        }else if (result == -1) {
+     UserDAO userDAO = new UserDAO();
+		int result = userDAO.login(user.getUserID(), user.getUserPassword());
+		if (result == 1) {
+			PrintWriter script = response.getWriter();
+			script.println("<script>");
+			script.println("location.href = 'main.jsp'");
+			script.println("</script>");
+		}
+		else if (result == 0) {
+			PrintWriter script = response.getWriter();
+			script.println("<script>");
+			script.println("alert('비밀번호가 틀립니다.')");
+			script.println("history.back()");
+			script.println("</script>");
+		}
+		else if (result == -1) {
 			PrintWriter script = response.getWriter();
 			script.println("<script>");
 			script.println("alert('존재하지 않는 아이디입니다.')");
 			script.println("history.back()");
 			script.println("</script>");
-		}else if(result == -2){
-        	PrintWriter script = response.getWriter();
-        	script.println("<script>");
-        	script.println("alert('데이터베이스 오류가 발생했습니다.')");
-        	script.println("history.back()"); //이전 페이지로 사용자를 돌려보냄  
-        	script.println("</script>");
+		}
+		else if (result == -2) {
+			PrintWriter script = response.getWriter();
+			script.println("<script>");
+			script.println("alert('데이터베이스 오류가 발생했습니다.')");
+			script.println("history.back()");
+			script.println("</script>");
         }
      %>
 </body>
